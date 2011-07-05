88c56fb7db47a621c3705d378dbaac45cffeeea5
Colin Alstad <colin.alstad@gmail.com>
2011-07-05 12:16:10 -0400

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clj-cloudq "1.0.0-SNAPSHOT"
+  :description "A clojure implementation of the cloudq server."
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/clj_cloudq/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-cloudq.test.core
+  (:use [clj-cloudq.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
