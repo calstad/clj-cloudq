0592353f9ebbcca423b7ce324c5d0eadf7166ca3
Mark Gunnels <markgunnels@gmail.com>
2011-07-06 05:52:39 -0400

    initial checkin

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
 (defproject clj-cloudq "1.0.0-SNAPSHOT"
   :description "A clojure implementation of the cloudq server."
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :resources-path "resources"
+  :uberjar-name "cloudq.jar"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [aleph "0.2.0-alpha3-SNAPSHOT"]
+                 [net.cgrand/moustache "1.0.0"]
+                 [congomongo "0.1.5-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
