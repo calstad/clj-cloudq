437fe10845b367b64f8e1f9de39fa5fa8ddbeb7b
Mark Gunnels <gunnels@musc.edu>
2011-07-06 09:14:25 -0400

    set up initial routes and rethought stack

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :resources-path "resources"
   :uberjar-name "cloudq.jar"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [aleph "0.2.0-alpha3-SNAPSHOT"]
-                 [net.cgrand/moustache "1.0.0"]
-                 [congomongo "0.1.5-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
+                 [compojure "0.6.4"]                 
+                 [congomongo "0.1.5-SNAPSHOT"]
+                 [ring-json-params "0.1.3"]
+                 [clj-json "0.3.2"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [lein-ring "0.4.5"]]
+  :ring {:handler clj-cloudq.core/app})
