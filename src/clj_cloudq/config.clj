e8c4b121e91aca2844986375d63df5c881f733ef
Colin Alstad <colin.alstad@gmail.com>
2011-07-06 22:55:20 -0400

    Add database config and connection intialization.

--- src/clj_cloudq/config.clj
+++ src/clj_cloudq/config.clj
@@ -1,4 +1,7 @@
 (ns clj-cloudq.config)
 
 (def mongodb
-     )
+  {:host "127.0.0.1"
+   :port "27017" 
+   :database "cloudq"
+   })
--- src/clj_cloudq/persist.clj
+++ src/clj_cloudq/persist.clj
@@ -1,2 +1,12 @@
 (ns clj-cloudq.persist
-  )
+   (:use somnium.congomongo)
+  (:require [clj-cloudq.config :as config]))
+
+(defn connect []
+  (let [conf config/mongodb]
+    (mongo! :db (:database conf) :host (:host conf) :port (Integer. (:port conf)))
+    (if (:user conf)
+      (authenticate (:user conf) (:password conf)))))
+
+
+
