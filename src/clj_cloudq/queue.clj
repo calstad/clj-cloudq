e0b83864c40d3b26f3158d3b973f55d3c0fbbb3c
Colin Alstad <colin.alstad@gmail.com>
2011-07-10 11:33:17 -0400

    Use config namespace if heroku env variables are not present.

--- src/clj_cloudq/queue.clj
+++ src/clj_cloudq/queue.clj
@@ -3,24 +3,25 @@
   (:use somnium.congomongo)
   (:require [clj-cloudq.config :as conf]))
 
-(defn get-config []
-  "Returns the config defined in the MONGOHQ_URL environment variable or the config namespace if the MongoHQ url does not exiest."
-  (let [mongo-url (get (System/getenv) "MONGOHQ_URL")]
-    (if mongo-url
-      (split-mongo-url mongo-url)
-      (conf/mongodb))))
-
 (defn split-mongo-url [url]
   "Parses mongodb url from heroku, eg. mongodb://user:pass@localhost:1234/db"
   (let [matcher (re-matcher #"^.*://(.*?):(.*?)@(.*?):(\d+)/(.*)$" url)]
     (when (.find matcher)
       (zipmap [:match :user :pass :host :port :db] (re-groups matcher)))))
 
+(defn get-config []
+  "Returns the config defined in the MONGOHQ_URL environment variable or the config namespace if the MongoHQ url does not exiest."
+  (let [mongo-url (get (System/getenv) "MONGOHQ_URL")]
+    (if mongo-url
+      (split-mongo-url mongo-url)
+      conf/mongodb)))
+
 (defn connect []
   "Connects to MongoDB"
   (let [config (get-config)]
     (mongo! :db (:db config) :host (:host config) :port (Integer. (:port config)))
-    (authenticate (:user config) (:pass config))))
+    (when (:user config)
+      (authenticate (:user config) (:pass config)))))
 
 ;;An Application Posts a JOB to a Cloudq Server Queue, that JOB is set to a state of :new.
 (defn add-job
