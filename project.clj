9fdf872765816914aef2e16a6e91603d295903ff
Colin Alstad <colin.alstad@gmail.com>
2011-07-12 22:57:19 -0400

    More work on routes tests.

--- project.clj
+++ project.clj
@@ -3,12 +3,14 @@
   :resources-path "resources"
   :uberjar-name "cloudq.jar"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.6.4"]                 
                  [congomongo "0.1.5-SNAPSHOT"]
                  [ring-json-params "0.1.3"]
                  [clj-json "0.3.2"]
                  [ring/ring-jetty-adapter "0.3.9"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [lein-ring "0.4.5"]]
+                     [lein-ring "0.4.5"]
+                     [ring-mock "0.1.1"]]
   :main clj-cloudq.core
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
--- test/clj_cloudq/test/routes.clj
+++ test/clj_cloudq/test/routes.clj
@@ -1,13 +1,23 @@
 (ns clj-cloudq.test.routes
-  (:use [clj-cloudq.routes])
-  (:use [clojure.test]))
+  (:use clj-cloudq.routes
+        clojure.test
+        ring.mock.request
+        clojure.contrib.mock.test-adapter)
+  (:require [clj-cloudq.queue :as q]))
 
-(defn request [resource web-app & params]
-  (web-app {:request-method :get :uri resource :params (first params)}))
+(defn cloudq-request
+  "Wrapper around ring mock request for convienence"
+  [method resource & job-params]
+  (main-routes (-> (request method resource) (assoc :params {"job" job-params}))))
 
-(deftest test-routes
-  (is (= 200 (:status (request "/" main-routes))))
-  (is (= "Hello from CloudQ" (:body (request "/" main-routes)))))
+(deftest root-url
+  (let [root-response (cloudq-request :get "/")]
+    (is (= 200 (:status root-response)))
+    (is (= "Hello from CloudQ" (:body root-response)))))
+
+(deftest post-job
+  (expect [q/add-job (times once (has-args ["test_queue" {:klass "TestClass" :args [{:args1 "test_args"}]}]))]
+          (cloudq-request :post "/test_queue" {:klass "TestClass" :args [{:args1 "test_args"}]})))
 
 (deftest json-responder-default-status
   (is (= 200 (:status (json-response {:foo "bar"})))))
@@ -17,3 +27,5 @@
 
 (deftest json-responder-content-type
   (is (= "application/json" (get (:headers (json-response {:foo "bar"})) "Content-Type"))))
+
+
